be92d7192178a5d30ec52a3d0b9bc66f0bbba097
0rland0Wats0n <ogw@orlandogwatson.com>
2015-10-10 12:56:27 -0400

    initial commit

---
+++ models/Hacker.js
@@ -0,0 +1,30 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Hacker Model
+ * ==========
+ */
+
+var Hacker = new keystone.List('Hacker');
+
+Hacker.add({
+	name: { type: Types.Name, required: true, index: true },
+	email: { type: Types.Email, initial: true, required: true, index: true },
+	password: { type: Types.Password, initial: true, required: true }
+}, 'Permissions', {
+	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
+});
+
+// Provide access to Keystone
+Hacker.schema.virtual('canAccessKeystone').get(function() {
+	return this.isAdmin;
+});
+
+
+/**
+ * Registration
+ */
+
+Hacker.defaultColumns = 'name, email, isAdmin';
+Hacker.register();
